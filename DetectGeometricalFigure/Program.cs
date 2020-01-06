703a68decb799fdb5026a540965073988a1e6df4
Sebastian <avertere1996@gmail.com>
2020-01-06 12:56:39 +0100

    Dodaj pliki projektów.

---
+++ DetectGeometricalFigure/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DetectGeometricalFigure
+{
+    class Program
+    {
+        static void Main()
+        {
+            string arg;
+            List<float> farg = new List<float>();
+            Console.WriteLine("Wprowadz boki figury (min 3 - max 4) odzielone jedna spacja:");
+            arg = Console.ReadLine();
+            string[] sarg = ValidateInput(arg);
+            farg = GetFloats(sarg).ToList();
+            var checksList = new List<Task>();
+            var checkTriangle = CheckTriangle(farg);
+            var checkQuadrangle = CheckQuadrangle(farg);
+
+            checksList.Add(checkTriangle);
+            checksList.Add(checkQuadrangle);
+            Task.WaitAll(checksList.ToArray());
+
+            if (String.IsNullOrEmpty(checkQuadrangle.Result) && String.IsNullOrEmpty(checkTriangle.Result))
+                Console.WriteLine("nierozpoznano");
+            else
+            {
+                if (!String.IsNullOrEmpty(checkQuadrangle.Result))
+                    Console.Write(checkQuadrangle.Result+" ");
+                if (!String.IsNullOrEmpty(checkTriangle.Result))
+                    Console.Write(checkTriangle.Result + " ");
+            }
+        }
+
+       static string[] ValidateInput(string arg)
+        {
+            string[] result = arg.Split(" ").Length < 3 || arg.Split(" ").Length > 4 ? null : arg.Split(" ");
+            if (result == null)
+            {
+                Console.WriteLine("Zła liczba boków - min 3 , max 4");
+                Environment.Exit(0);
+            }
+
+            if (result.Any(x => String.IsNullOrEmpty(x)))
+            {
+                Console.WriteLine("Zły format danych");
+                Environment.Exit(0);
+            }
+            return result;
+        }
+
+       static IEnumerable<float> GetFloats(string[] sarg)
+       {
+            foreach(var s in sarg)
+            {
+                float f;
+                if (!float.TryParse(s, out f))
+                {
+                    Console.WriteLine("Argumenty muszą być liczbami");
+                    Environment.Exit(0);
+                }
+                else
+                    yield return f;
+            }
+       }
+
+        static public  Task<string> CheckTriangle(List<float> args)
+        {
+            return Task.Run(() =>
+            {
+                if (args.Count == 3)
+                {
+                    float a = args[0], b = args[1], c = args[2];
+
+                    if ((a + b > c) || (a + c > b) || (b + c > a))
+                    {
+                        if ((a == b) && (a == c) && (b == c))
+                            return "Można zbudować trójkąt równoramienny i równoboczny";
+                        else if ((a == b) || (b == c) || (c == a))
+                            return "Można zbudować trójkąt równoboczny";
+
+                        return "Można zbudować trójkąt różnoramienny";
+
+                    }
+                    else
+                        return null;//Nie można zbudować trójkąta z podanych boków
+                }
+                else
+                    return null;//Nie można zbudować trójkąta z czterech boków
+            });
+        }
+
+        static public Task<string> CheckQuadrangle(List<float> args)
+        {
+            return Task.Run(() =>
+            {
+                if (args.Count == 4)
+                {
+                    string result = "";
+                    float a = args[0], b = args[1], c = args[2], d = args[3];
+                    if (a == b && b == c && c == d)
+                        result += "Można zbudować kwadrat i prostokąt";
+                    else if ((a == b && c == d) || (a == c && b == d) || (d == a && b == c))
+                        result += "Można zbudować prostokąt";
+                    return result + " Można zbudować czworobok";
+
+                }
+                else
+                    return null;//Nie można zbudować czworoboku z trzech boków
+            });
+        }
+    }
+}
